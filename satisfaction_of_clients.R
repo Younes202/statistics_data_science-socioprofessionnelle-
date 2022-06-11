c51099426ce1c8377a69cfdc96f82e0faddfe25c
“Younes <sghyeryounes@gmail.com>
2022-06-11 11:10:17 +0200

    graphic - representation

--- satisfaction_of_clients.R
+++
@@ -1,8 +0,0 @@
-
-> n_clients<-c(700,512,703,385,200)
-> satisfaction<-c("Pas du tout satisfait","Pas satisfait","Moyennement satisfait","satisfait","tres satisfait")
-> pct<-round(n_clients/sum(n_clients)*100)
-> satisfaction<-paste(satisfaction,pct)
-> satisfaction<-paste(satisfaction,"%",sep="")
-> pie(n_clients,labels=satisfaction, col=rainbow(length(satisfaction)), main="degré de satisfaction ")
-
