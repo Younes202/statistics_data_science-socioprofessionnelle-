9368d19f0e48ce527c6919e724ac959aba7ccc70
“Younes <sghyeryounes@gmail.com>
2022-06-11 12:19:37 +0200

    style added

--- Ex_1.R
+++
@@ -1,7 +0,0 @@
-//1)// 1) Population : active dès 15 ans ou plus en 2009 , taille = 29,783 type= Qualitatif nominale
-> n_clients<-c(700,512,703,385,200)
-> satisfaction<-c("Pas du tout satisfait","Pas satisfait","Moyennement satisfait","satisfait","tres satisfait")
-> pct<-round(n_clients/sum(n_clients)*100)
-> satisfaction<-paste(satisfaction,pct)
-> satisfaction<-paste(satisfaction,"%",sep="")
-> pie(n_clients,labels=satisfaction, col=rainbow(length(satisfaction)), main="degré de satisfaction ")
--- socioprofessionnelle.R
+++ socioprofessionnelle.R
@@ -1,4 +1,8 @@
 >gfg <- data.frame(x = c(365,145,1238,494,2750,1746,3443,3781,2037,6700,5719,1365), 
 +  grp = rep(c("Arg", "Arts","Card","Pr in","Emp","Ouv"),each = 2),
 + subgroup = LETTERS[1:2])
+>gfg <- reshape(gfg,idvar = "subgroup",
++                timevar = "grp",
++                direction = "wide")
+>  
 
