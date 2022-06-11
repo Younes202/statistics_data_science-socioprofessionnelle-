7d82eec255c4240e53acc4ccb4b6af5bd97d92ed
“Younes <sghyeryounes@gmail.com>
2022-06-11 12:09:25 +0200

    prepare_data

---
+++ socioprofessionnelle.R
@@ -0,0 +1,4 @@
+>gfg <- data.frame(x = c(365,145,1238,494,2750,1746,3443,3781,2037,6700,5719,1365), 
++  grp = rep(c("Arg", "Arts","Card","Pr in","Emp","Ouv"),each = 2),
++ subgroup = LETTERS[1:2])
+
